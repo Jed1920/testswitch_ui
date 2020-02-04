b8a89e59d57ed412baba823215098eca7945ebdd
Jed Johnson <jed.johnson@softwire.com>
2020-02-04 17:43:23 +0000

    Created failing test

--- src/App.test.js
+++ src/App.test.js
@@ -7,3 +7,8 @@ test('renders learn react link', () => {
   const linkElement = getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+test('failed test', () => {
+  const { getByText } = render(<App />);
+  expect(getByText(bbb)).toBeInTheDocument();
+});
