67a9fdd365847cc0065b905f6a582c013d5bd8c6
Jed Johnson <jed.johnson@softwire.com>
2020-02-17 15:43:13 +0000

    Created fetch function for get all applicants

--- src/General/apiFetch.js
+++ src/General/apiFetch.js
@@ -7,3 +7,9 @@ export async function addApplicantFetch(formData){
     })
     return await response
 }
+
+export async function getFetch(){
+    var url = `${process.env.REACT_APP_API_URL}/application/get_all`
+    const response = await fetch(url)
+    return await response
+}
