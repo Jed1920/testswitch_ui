b6b10408c8118ebc14123d5697629f79f2017f81
Jed Johnson <jed.johnson@softwire.com>
2020-02-19 18:30:27 +0000

    Created file for buttons

---
+++ src/AdminManagementPage/ApplicationState.js
@@ -0,0 +1,52 @@
+import React, { useState }from 'react'
+import './ApplicationState.scss'
+import { getFetch } from '../General/apiFetch'
+
+export const listAction = {
+    UPDATE:"update",
+    NO_UPDATE:"no update"
+}
+
+
+export function ApplicantButton(props){
+    props.setUpdateList(listAction.NO_UPDATE)
+
+    async function handleClick(state){
+        props.setUpdateList(listAction.UPDATE)
+        console.log(`${props.applicant.id}/${state}`)
+        await getFetch(`/application/change_state/${props.applicant.id}/${state}`)
+    }
+
+    switch(props.applicant.applicationState){
+        case "NEW":
+            return( 
+                <div className = "buttons">
+                    <button className = "sendButton" onClick={()=>{handleClick("SENT")}}>Send Test</button>
+                    <button className = "rejectButton" onClick={()=>{handleClick("REJECTED")}}>Reject</button>
+                </div>)
+        case "SENT":
+            return ( 
+                <div className = "buttons">
+                    <p className ="testText">Sent</p>
+                    <button className = "rejectButton" onClick={()=>{handleClick("REJECTED")}}>Reject</button>
+                </div>)
+        case "EXPIRED":
+            return ( 
+                <div className = "buttons">
+                    <p className ="testText">Expired</p>
+                    <button className = "rejectButton" onClick={()=>{handleClick("REJECTED")}}>Reject</button>
+                </div>)
+        case "COMPLETED":
+            return ( 
+                <div className = "buttons">
+                    <p className ="testText">Completed</p>
+                    <button className = "rejectButton" onClick={()=>{handleClick("REJECTED")}}>Reject</button>
+                    <button className = "acceptButton" onClick={()=>{handleClick("ACCEPTED")}}>Accept</button>
+                </div>)
+        case "REJECTED":
+            return <p className ="testText">Rejected</p>
+        case "ACCEPTED":
+            return <p className ="testText">Accepted</p>
+
+    }
+}
