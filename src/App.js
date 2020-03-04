da9a4b9c35433970e5e3f1b2fae0b5f53911332b
Jed Johnson <jed.johnson@softwire.com>
2020-03-04 18:23:00 +0000

    Created homepage component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import { BrowserRouter as Router,Switch,Route } from 'react-router-dom';
-import './App.css';
 import {Register} from './RegisterPage/Register'
 import { AdminManagement } from './AdminManagementPage/AdminManagement';
 import { ApplicantTest } from './TestPage/ApplicantTest';
+import { Navbar } from './Navbar/Navbar';
+import { Homepage } from './Homepage/Homepage';
 
 function App() {
   require('dotenv').config();
   return (
 <Router>
+  <Navbar/>
   <Switch>
 
     <Route exact path="/">
-      <div>Homepage</div>
+      <Homepage/>
     </Route>
 
     <Route exact path="/register">
