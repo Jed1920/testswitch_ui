2f4a26971b8c0111dad2962873569c8123a3a81d
Jed Johnson <jed.johnson@softwire.com>
2020-03-04 23:02:18 +0000

    Styled homepage

--- src/Homepage/Homepage.js
+++ src/Homepage/Homepage.js
@@ -4,7 +4,22 @@ import './Homepage.scss'
 export function Homepage(){
     return (
         <div className="homepage">
-            Homepage
+            <section className="topSection">
+                <img src="/images/computerImage04.jpg" alt="background image" className="backgroundImage"></img>
+                <div className="introPara">
+                    <h2>Programming Bootcamp</h2>
+                    <p>Up unpacked friendly ecstatic so possible humoured do. Ample end might folly quiet one set spoke her. We no am former valley assure. 
+                        Four need spot ye said we find mile. Are commanded him convinced dashwoods did estimable forfeited. Shy celebrated met sentiments 
+                        she reasonably but. Proposal its disposed eat advanced marriage sociable. 
+                    </p>
+                    <p>Drawings led greatest add subjects endeavor gay remember. 
+                        Principles one yet assistance you met impossible.She suspicion dejection saw instantly. Well deny may real one told yet saw hard 
+                        dear. Bed chief house rapid right the. Set noisy one state tears which. No girl oh part must fact high my he. Simplicity in 
+                        excellence melancholy as remarkably discovered. Own partiality motionless was old excellence she inquietude contrasted.
+                    </p>
+                </div>
+            </section>
+            
         </div>
     )
 }
